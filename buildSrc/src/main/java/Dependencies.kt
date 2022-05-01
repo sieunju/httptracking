a395f18eaa37a2e95818af302971b51d8acb580a
sieunju <j.sieun73@gmail.com>
2022-05-01 11:10:29 +0900

    chore: 재배포 - root build gradle 에 jitpack 추가

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,8 +3,8 @@ object Apps {
     const val buildToolsVersion = "31.0.0"
     const val minSdkVersion = 21
     const val targetSdkVersion = 31
-    const val versionCode = 7
-    const val versionName = "1.0.5"
+    const val versionCode = 8
+    const val versionName = "1.0.6"
 }
 
 object Versions {
