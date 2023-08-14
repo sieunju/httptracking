1192dbf156f8c0e48b89f2f81131a5e43f2f3f29
sieunju <j.sieun73@gmail.com>
2023-08-14 09:54:17 +0900

    refactor: 검색 기능 개선 - Flow 를 이용해서 TextChangeListener 처리

--- httptracking/src/main/java/hmju/http/tracking/ui/list/TrackingListFragment.kt
+++ httptracking/src/main/java/hmju/http/tracking/ui/list/TrackingListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.coroutineScope
@@ -20,9 +21,15 @@ import hmju.http.tracking.ui.adapter.TrackingAdapter
 import hmju.http.tracking_interceptor.TrackingDataManager
 import hmju.http.tracking_interceptor.model.TrackingModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -36,7 +43,7 @@ internal class TrackingListFragment : Fragment(R.layout.f_tracking_list) {
     private lateinit var rvContents: RecyclerView
     private lateinit var etKeyword: AppCompatEditText
     private lateinit var appBarLayout: AppBarLayout
-    private val currentKeyword: MutableStateFlow<String> by lazy { MutableStateFlow("") }
+    // private val currentKeyword: MutableStateFlow<String> by lazy { MutableStateFlow("") }
 
     private val adapter: TrackingAdapter by lazy { TrackingAdapter(this) }
 
@@ -49,7 +56,7 @@ internal class TrackingListFragment : Fragment(R.layout.f_tracking_list) {
         rvContents.layoutManager = LinearLayoutManager(view.context)
         rvContents.adapter = adapter
 
-        setSearchKeyword()
+        initSearchKeyword()
         setTrackingData(TrackingDataManager.getInstance().getTrackingList())
 
         TrackingDataManager.getInstance().setListener(object : TrackingDataManager.Listener {
@@ -74,12 +81,47 @@ internal class TrackingListFragment : Fragment(R.layout.f_tracking_list) {
         }
     }
 
-    private fun setSearchKeyword() {
-        lifecycleScope.launchWhenResumed {
-            currentKeyword.collectLatest {
-                delay(100)
-                searchTrackingList(it)
+    /**
+     * Flow 를 활용한 EditText Change Observer 함수
+     */
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun EditText.textChangeObserver(): Flow<CharSequence?> {
+        return callbackFlow {
+            val listener = object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    trySend(s)
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
             }
+            addTextChangedListener(listener)
+            awaitClose { removeTextChangedListener(listener) }
+        }.onStart {
+            // 콜백이 시작 될때 Event 방출
+            emit(text)
+        }
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun initSearchKeyword() {
+        lifecycleScope.launch {
+            etKeyword.textChangeObserver()
+                .debounce(100)
+                .onEach { searchTrackingList(it.toString()) }
+                .launchIn(this)
+        }
+        /*lifecycleScope.launchWhenResumed {
+            currentKeyword
+                .debounce(100)
+                .collectLatest { searchTrackingList(it) }
         }
         etKeyword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -89,7 +131,7 @@ internal class TrackingListFragment : Fragment(R.layout.f_tracking_list) {
             override fun afterTextChanged(s: Editable?) {
                 currentKeyword.value = s.toString()
             }
-        })
+        })*/
     }
 
     /**
